e9200a908681f6fec771eed0c38fe9f9c327f231
mhamed <mhamedled@gmail.com>
2022-07-20 15:23:02 +0100

    first commit

---
+++ base/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views 
+
+urlpatterns = [
+
+    path('', views.getRoute),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>', views.getRoom),
+]
